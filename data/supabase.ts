48bb149a3e80bb216f3030c4daa4aeec1dd9e74e
Dylan Verallo <61432427+Kurenai-Misuzu@users.noreply.github.com>
2025-06-03 22:38:46 -0700

    added start of supabase and react query

---
+++ data/supabase.ts
@@ -0,0 +1,20 @@
+import { createClient } from "@supabase/supabase-js";
+
+let supabaseURL: string;
+let supabaseKEY: string;
+
+export const checkEnv = () => {};
+// typechecking keys
+if (process.env.EXPO_PUBLIC_SUPABASE_URL) {
+  supabaseURL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+} else {
+  throw new Error("No supabase URL");
+}
+if (process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY) {
+  supabaseKEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+} else {
+  throw new Error("No supabase Key");
+}
+
+// create client
+export const supabase = createClient(supabaseURL, supabaseKEY);
